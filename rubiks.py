Fixed rotation methods and added testing class

rubiks.py
@@ -9,15 +9,19 @@ arbitrary sized Rubik's cube.
 We assume that:
     Blue is the front face
     Green is the back
-    White is the right
-    Yellow is the left
-    Red is the top
-    Orange is the bottom
+    Red is the right
+    Orange is the left
+    White is the top
+    Yellow is the bottom
 You can change this by changing the character values in the initialization.
 
 Note that rows are 0 to n-1, top down.
           cols are 0 to n-1, left right.
           depths are 0 to n-1 front back.
+
+To see the orientation of each face, hold the cub from the perspective of the front.
+Spinning left to right, shows the proper orientiation of the front, right, back, and left faces.
+For the top, the bottom row is the one touching the front edge. This keeps the depth definition consistent.
 '''
 
 from random import randint
@@ -25,7 +29,7 @@ import numpy as np
 
 
 class Cube:
-    def __init__(self, n, front='B', back='G', left='Y', right='W', top='R', bottom='O'):
+    def __init__(self, n, front='B', back='G', left='R', right='O', top='W', bottom='Y'):
         self.n = n
         self.front_face = np.full((n,n), front)
         self.back_face = np.full((n,n), back)
@@ -39,13 +43,18 @@ class Cube:
 
         self.max_rotations = 0
 
-    def set_faces(self, faces):
-        self.front_face = faces[0]
-        self.back_face = faces[1]
-        self.left_face = faces[2]
-        self.right_face = faces[3]
-        self.top_face = faces[4]
-        self.bottom_face = faces[5]
+    # Sets the faces of the cube.
+    def set_faces(self, front, back, left, right, top, bottom):
+        self.front_face = front
+        self.back_face = back
+        self.left_face = left
+        self.right_face = right
+        self.top_face = top
+        self.bottom_face = bottom
+
+    # Returns the faces of the cube.
+    def get_faces(self):
+        return [self.front_face, self.back_face, self.left_face, self.right_face, self.top_face, self.bottom_face]
 
 
     # Shuffles the cube for r rotations.
@@ -59,26 +68,44 @@ class Cube:
             max_rotations = rotations
 
 
-    # Rotates a row (horizontally, x), 1 is right, and -1 is left.
+    # Rotates a row (horizontally, x), 1 is left, and -1 is right.
     def rotate_row(self, row, direction):
         rows = np.append(self.front_face[row], [self.left_face[row], self.back_face[row], self.right_face[row]])
         rows = np.roll(rows, direction*self.n)
         (self.front_face[row], self.left_face[row], self.back_face[row], self.right_face[row]) = np.split(rows, 4)
 
+        if row == 0:
+            self.top_face = np.rot90(self.top_face, -direction)     
+        elif row == self.n - 1:
+            self.bottom_face = np.rot90(self.bottom_face, direction)
 
-    # Rotates a column (vertically, y) (1 backwards and -1 is forwards)
+
+    # Rotates a column (vertically, y) (1 forwards and -1 is backwards)
     def rotate_column(self, col, direction):
-        cols = np.append(self.front_face[:,col], [self.top_face[:,col], self.back_face[:,col], self.bottom_face[:,col]])
+        cols = np.append(self.front_face[:,col], [np.flip(self.bottom_face[:,self.n-1-col]), np.flip(self.back_face[:,self.n-1-col]), self.top_face[:,col]])
         cols = np.roll(cols, direction*self.n)
-        (self.front_face[:,col], self.top_face[:,col], self.back_face[:,col], self.bottom_face[:,col]) = np.split(cols, 4)
+        (self.front_face[:,col], self.bottom_face[:,self.n-1-col], self.back_face[:,self.n-1-col], self.top_face[:,col]) = np.split(cols, 4)
+        self.bottom_face[:,self.n-1-col] = np.flip(self.bottom_face[:,self.n-1-col])
+        self.back_face[:,self.n-1-col] = np.flip(self.back_face[:,self.n-1-col])
+
+        if col == 0:
+            self.left_face = np.rot90(self.left_face, -direction)
+        elif col == self.n -1:
+            self.right_face = np.rot90(self.right_face, direction)
 
 
     # Rotate a depth layer (z) (1 counterclockwise, -1 clockwise)
     # Note that depth corresponds to a row on some faces and columns on others.
     def rotate_layer(self, depth, direction):
-        depths = np.append(self.top_face[-depth-1], [self.left_face[:,-depth-1], self.bottom_face[depth], self.right_face[:,depth]])
-        depths = np.roll(depths, direction*self.n)
-        (self.top_face[-depth-1], self.left_face[:,-depth-1], self.bottom_face[depth], self.right_face[:,depth]) = np.split(depths, 4)
+        depths = np.append(self.top_face[-depth-1], [self.right_face[:,depth], self.bottom_face[-depth-1], np.flip(self.left_face[:,-depth-1])])
+        depths = np.roll(depths, -direction*self.n)
+        (self.top_face[-depth-1], self.right_face[:,depth], self.bottom_face[-depth-1], self.left_face[:,-depth-1]) = np.split(depths, 4)
+        self.left_face[:,-depth-1] = np.flip(self.left_face[:,-depth-1])
+
+        if depth == 0:
+            self.front_face = np.rot90(self.front_face, direction)
+        elif depth == self.n -1:
+            self.back_face = np.rot90(self.back_face, -direction)
 
     def print_cube(self):
         print ("Front:\n", self.front_face)

test.py
@@ -1,19 +1,135 @@
 from rubiks import Cube
 import numpy as np
+import unittest
 
-'''
-# Example to shuffle cube.
-cube = Cube(3)
-cube.print_cube()
-cube.shuffle_cube(2)
-print()
-cube.print_cube()
-'''
+ 
+class Test(unittest.TestCase):
+    # Ensure that rotating top row left changes cube correctly.
+    def test_1(self):
+        cube = Cube(3)
+        front = np.array([['W','G','W'],['O','B','O'],['Y','B','Y']])
+        right = np.array([['O','B','O'],['W','O','Y'],['O','G','O']])
+        back = np.array([['W','B','W'],['R','G','R'],['Y','G','Y']])
+        left = np.array([['R','G','R'],['W','R','Y'],['R','B','R']])
+        top = np.array([['B','Y','B'],['O','W','R'],['G','Y','G']])
+        bottom = np.array([['B','W','B'],['R','Y','O'],['G','W','G']])
+        cube.set_faces(front, back, left, right, top, bottom)
 
-# Example to load dataset and initialize cube.
-arr =  np.load('./testing/size_2.npy')
-faces1 = arr[0]
-print(faces1)
-cube = Cube(len(faces1[0]))
-cube.set_faces(faces1)
-cube.print_cube()
+        front = np.array([['O','B','O'],['O','B','O'],['Y','B','Y']])
+        right = np.array([['W','B','W'],['W','O','Y'],['O','G','O']])
+        back = np.array([['R','G','R'],['R','G','R'],['Y','G','Y']])
+        left = np.array([['W','G','W'],['W','R','Y'],['R','B','R']])
+        top = np.array([['G','O','B'],['Y','W','Y'],['G','R','B']])
+        bottom = np.array([['B','W','B'],['R','Y','O'],['G','W','G']])
+        cube.rotate_row(0, 1)
+
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+    
+    # Test rotating bottom row right.
+    def test_2(self):
+        cube = Cube(3)
+        front = np.array([['W','G','W'],['O','B','O'],['Y','B','Y']])
+        right = np.array([['O','B','O'],['W','O','Y'],['O','G','O']])
+        back = np.array([['W','B','W'],['R','G','R'],['Y','G','Y']])
+        left = np.array([['R','G','R'],['W','R','Y'],['R','B','R']])
+        top = np.array([['B','Y','B'],['O','W','R'],['G','Y','G']])
+        bottom = np.array([['B','W','B'],['R','Y','O'],['G','W','G']])
+        cube.set_faces(front, back, left, right, top, bottom)
+
+        front = np.array([['W','G','W'],['O','B','O'],['R','B','R']])
+        right = np.array([['O','B','O'],['W','O','Y'],['Y','B','Y']])
+        back = np.array([['W','B','W'],['R','G','R'],['O','G','O']])
+        left = np.array([['R','G','R'],['W','R','Y'],['Y','G','Y']])
+        top = np.array([['B','Y','B'],['O','W','R'],['G','Y','G']])
+        bottom = np.array([['G','R','B'],['W','Y','W'],['G','O','B']])
+        cube.rotate_row(2, -1)
+
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+
+    # Test rotating left column forwards.
+    def test_3(self):
+        cube = Cube(3)
+        front = np.array([['W','G','W'],['O','B','O'],['Y','B','Y']])
+        right = np.array([['O','B','O'],['W','O','Y'],['O','G','O']])
+        back = np.array([['W','B','W'],['R','G','R'],['Y','G','Y']])
+        left = np.array([['R','G','R'],['W','R','Y'],['R','B','R']])
+        top = np.array([['B','Y','B'],['O','W','R'],['G','Y','G']])
+        bottom = np.array([['B','W','B'],['R','Y','O'],['G','W','G']])
+        cube.set_faces(front, back, left, right, top, bottom)
+
+        front = np.array([['B','G','W'],['O','B','O'],['G','B','Y']])
+        right = np.array([['O','B','O'],['W','O','Y'],['O','G','O']])
+        back = np.array([['W','B', 'B'],['R','G','O'],['Y','G','G']])
+        left = np.array([['R','W','R'],['B','R','G'],['R','Y','R']])
+        top = np.array([['Y','Y','B'],['R','W','R'],['W','Y','G']])
+        bottom = np.array([['B','W','Y'],['R','Y','O'],['G','W','W']])
+        cube.rotate_column(0, 1)
+
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+
+    # Test rotating right column backwards.
+    def test_4(self):
+        cube=Cube(3)
+        front = np.array([['W','G','W'],['O','B','O'],['Y','B','Y']])
+        right = np.array([['O','B','O'],['W','O','Y'],['O','G','O']])
+        back = np.array([['W','B','W'],['R','G','R'],['Y','G','Y']])
+        left = np.array([['R','G','R'],['W','R','Y'],['R','B','R']])
+        top = np.array([['B','Y','B'],['O','W','R'],['G','Y','G']])
+        bottom = np.array([['B','W','B'],['R','Y','O'],['G','W','G']])
+        cube.set_faces(front, back, left, right, top, bottom)
+
+        front = np.array([['W','G','G'],['O','B','R'],['Y','B','B']])
+        right = np.array([['O','W','O'],['G','O','B'],['O','Y','O']])
+        back = np.array([['G','B','W'],['R','G','R'],['B','G','Y']])
+        left = np.array([['R','G','R'],['W','R','Y'],['R','B','R']])
+        top = np.array([['B','Y','W'],['O','W','O'],['G','Y','Y']])
+        bottom = np.array([['W','W','B'],['R','Y','O'],['Y','W','G']])
+        cube.rotate_column(2, -1)
+        
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+
+    # Test rotating front layer counterclockwise
+    def test_5(self):
+        cube=Cube(3)
+        front = np.array([['W','R','O'],['O','B','Y'],['G','R','R']])
+        right = np.array([['Y','R','W'],['G','O','G'],['B','W','B']])
+        back = np.array([['G','O','R'],['O','G','Y'],['W','R','O']])
+        left = np.array([['B','O','G'],['B','R','W'],['Y','W','Y']])
+        top = np.array([['W','B','R'],['Y','W','B'],['O','Y','G']])
+        bottom = np.array([['O','W','B'],['G','Y','B'],['Y','G','R']])
+        cube.set_faces(front, back, left, right, top, bottom)
+
+        front = np.array([['O','Y','R'],['R','B','R'],['W','O','G']])
+        right = np.array([['Y','R','W'],['G','O','G'],['R','W','B']])
+        back = np.array([['G','O','R'],['O','G','Y'],['W','R','O']])
+        left = np.array([['B','O','G'],['B','R','Y'],['Y','W','O']])
+        top = np.array([['W','B','R'],['Y','W','B'],['Y','G','B']])
+        bottom = np.array([['O','W','B'],['G','Y','B'],['Y','W','G']])
+        cube.rotate_layer(0, 1)
+
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+
+    def test_6(self):
+        cube=Cube(3)
+        front = np.array([['W','R','O'],['O','B','Y'],['G','R','R']])
+        right = np.array([['Y','R','W'],['G','O','G'],['B','W','B']])
+        back = np.array([['G','O','R'],['O','G','Y'],['W','R','O']])
+        left = np.array([['B','O','G'],['B','R','W'],['Y','W','Y']])
+        top = np.array([['W','B','R'],['Y','W','B'],['O','Y','G']])
+        bottom = np.array([['O','W','B'],['G','Y','B'],['Y','G','R']])
+        cube.set_faces(front, back, left, right, top, bottom)
+
+        front = np.array([['W','R','O'],['O','B','Y'],['G','R','R']])
+        right = np.array([['Y','R','W'],['G','O','B'],['B','W','R']])
+        back = np.array([['R','Y','O'],['O','G','R'],['G','O','W']])
+        left = np.array([['B','O','G'],['W','R','W'],['O','W','Y']])
+        top = np.array([['Y','B','B'],['Y','W','B'],['O','Y','G']])
+        bottom = np.array([['W','G','B'],['G','Y','B'],['Y','G','R']])
+        cube.rotate_layer(2, -1)
+
+        np.testing.assert_array_equal(cube.get_faces(), [front,back,left,right,top,bottom], "Should be True")
+
+
+if __name__ == '__main__':
+    unittest.main()
+ 