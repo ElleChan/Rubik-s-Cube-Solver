eeaa59d6adde9685af316bc674bd9e75fc877044
ElleChan <programmer.jael.butler@gmail.com>
2019-11-17 18:38:22 -0700

    Improved dataset (small subsample included)

--- gen_datasets.py
+++ gen_datasets.py
@@ -3,37 +3,59 @@ Jael Butler
 CS 4820
 10/23/2019
 
-This script generates all of the needed datasets and saves them to a binary file.
-We generate 1000 instances for each shuffle count away from the goal for the training set.
-And 10 instances of each shuffle count for the testing.
+
 '''
 from rubiks import Cube
 import numpy as np
+import csv
 
+# Creates a dataset file.
+def create_set(n, shuffles, count, dir):
+    print("Generating set for size", n)
+    f_name = dir + '/size_' + str(n) + '.csv'
+    handle = open(f_name, 'w', newline='')
+    writer = csv.writer(handle)
 
-def create_set(sizes, iters, dir):
-    for n in sizes:
-        print("Generating set for size", n)
-        file = dir + '/size_' + str(n) + '.npy'
+    cubes = {}
+    for c in range(count):          
+        for s in range(1, shuffles+1):
+            cube = Cube(n, 1, 2, 3, 4, 5, 6)            # Choose numbers for faces instead of colors
+            cube.shuffle_cube(s)
+            faces = cube.get_faces()
+            row = tuple(np.stack(faces).flatten())
 
-        cube_arr = []
-        for s in range(0,shuffles+1):
-            for iter in range(0,iters):
-                cube = Cube(n)
+            while row in cubes.keys():                # Avoid duplicate cubes.
+                if cube.max_rotations < cubes[row]:
+                    cubes[row] = cube.max_rotations
+                cube = Cube(n, 1, 2, 3, 4, 5, 6)      
                 cube.shuffle_cube(s)
-                faces = [cube.front_face, cube.back_face, cube.left_face, cube.right_face, cube.top_face, cube.bottom_face]
-                cube_arr.append(faces)
+                faces = cube.get_faces()
+                row = tuple(np.stack(faces).flatten())
 
-        np.save(file, cube_arr)
+            cubes[row] = cube.max_rotations    
+            output = list(row)
+            output.append(cube.max_rotations)
+            writer.writerow(output)
 
-        # Ensure load.
-        arr = np.load(file)
-        print(arr)
+    # Add solved cube.
+    cube = Cube(n, 1,2,3,4,5,6)
+    faces = cube.get_faces()
+    row = tuple(np.stack(faces).flatten())
+    output = list(row)
+    output.append(cube.max_rotations)
+    writer.writerow(output)
 
+    # Close handle.
+    handle.close()
 
-#sizes = (2,3,4,5,6,7)
-sizes = (2,3,4)
-shuffles = 26
+# Create training sets.
+#create_set(2, 26, 1000, './training')
+#create_set(3, 26, 1000, './training')
+create_set(2, 26, 1, './training')
+create_set(3, 26, 1, './training')
 
-create_set(sizes, 1000, './training')
-create_set(sizes, 10, './testing')
+# Create testing sets.
+#create_set(2, 26, 10, './testing')
+#create_set(3, 26, 10, './testing')
+create_set(2, 26, 1, './testing')
+create_set(3, 26, 1, './testing')
