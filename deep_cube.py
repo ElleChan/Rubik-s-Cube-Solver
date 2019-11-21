2a03c240b24957deae027dd41d31310d72520bec
ElleChan <programmer.jael.butler@gmail.com>
2019-11-21 10:39:15 -0700

    Working DNN prototype

--- deep_cube.py
+++ deep_cube.py
@@ -4,11 +4,43 @@ Class: CS 4820
 Duedate: 10/ /2019
 
 This module contains the code for the Deep Cube.
+Referenced: https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+            to learn how to use Keras.
 '''
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import to_categorical
 
-# Converts a cube to a proper input vector.
-def convertCube():
-    pass
+sizes = [2]
+
+
+for n in sizes:
+    print("\nTesting size", n, "\n")
+
+    # Get training data.
+    training_set = np.genfromtxt('./datasets/training/size'+str(n)+'.csv', delimiter=',')
+    x_train, y_train = np.hsplit(training_set, np.array([-1]))
+    y_train = to_categorical(y_train)
+    input_size = len(x_train[0])
+    #print(x_train, y_train)
+
+    # Define DNN model
+    model = Sequential()
+    model.add(Dense(input_size // 2, input_dim=input_size, activation='relu'))       # Hidden layer 1.
+    model.add(Dense(31, activation='sigmoid'))                                       # Output layer.
+    
+    # Compile DNN.
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Train the DNN
+    model.fit(x_train, y_train, epochs=10, batch_size=1000)
+
+    # Test DNN
+    testing_set = np.genfromtxt('./datasets/testing/size'+str(n)+'.csv', delimiter=',')
+    x_test, y_test = np.hsplit(testing_set, np.array([-1]))
+    y_test = to_categorical(y_test)
+    _, accuracy = model.evaluate(x_test, y_test)
+    print(accuracy*100, "%")
+
+    
--- gen_datasets.py
+++ gen_datasets.py
@@ -10,52 +10,59 @@ import numpy as np
 import csv
 
 # Creates a dataset file.
-def create_set(n, shuffles, count, dir):
+def create_set(n, shuffles, count, split):
     print("Generating set for size", n)
-    f_name = dir + '/size_' + str(n) + '.csv'
-    handle = open(f_name, 'w', newline='')
-    writer = csv.writer(handle)
+
+    train_handle = open('./datasets/training/size'+str(n)+'.csv', 'w', newline='')
+    test_handle = open('./datasets/testing/size'+str(n)+'.csv', 'w', newline='')
+    train_writer = csv.writer(train_handle)
+    test_writer = csv.writer(test_handle)
 
     cubes = {}
-    for c in range(count):          
-        for s in range(1, shuffles+1):
+    for c in range(count):
+        if c % 100 == 0:
+            print("\t", c)          
+        for s in range(0, shuffles+1):
             cube = Cube(n, 1, 2, 3, 4, 5, 6)            # Choose numbers for faces instead of colors
             cube.shuffle_cube(s)
             faces = cube.get_faces()
             row = tuple(np.stack(faces).flatten())
 
-            while row in cubes.keys():                # Avoid duplicate cubes.
+            # Get best fit.
+            if row in cubes.keys():
                 if cube.max_rotations < cubes[row]:
                     cubes[row] = cube.max_rotations
-                cube = Cube(n, 1, 2, 3, 4, 5, 6)      
-                cube.shuffle_cube(s)
-                faces = cube.get_faces()
-                row = tuple(np.stack(faces).flatten())
-
-            cubes[row] = cube.max_rotations    
-            output = list(row)
-            output.append(cube.max_rotations)
-            writer.writerow(output)
+            else:
+                cubes[row] = cube.max_rotations
 
     # Add solved cube.
     cube = Cube(n, 1,2,3,4,5,6)
     faces = cube.get_faces()
     row = tuple(np.stack(faces).flatten())
-    output = list(row)
-    output.append(cube.max_rotations)
-    writer.writerow(output)
+    cubes[row] = 0
+
+    #  Split dataset.
+    keys = list(cubes.keys())
+    training_set = keys[(split-1):]
+    testing_set = keys[-split:]
+
+
+    # Write datasets.
+    for tr_cube in training_set:
+        output = list(tr_cube)
+        output.append(cubes[tr_cube])
+        train_writer.writerow(output)
+    for tst_cube in testing_set:
+        output = list(tst_cube)
+        output.append(cubes[tst_cube])
+        test_writer.writerow(output)
 
     # Close handle.
-    handle.close()
+    train_handle.close()
+    test_handle.close()
 
 # Create training sets.
 #create_set(2, 26, 1000, './training')
 #create_set(3, 26, 1000, './training')
-create_set(2, 26, 1, './training')
-create_set(3, 26, 1, './training')
-
-# Create testing sets.
-#create_set(2, 26, 10, './testing')
-#create_set(3, 26, 10, './testing')
-create_set(2, 26, 1, './testing')
-create_set(3, 26, 1, './testing')
+create_set(2, 30, 2000, 100)      # 12 is the max for rot 1.
+#create_set(3, 26, 5, './datasets/size3.csv')
